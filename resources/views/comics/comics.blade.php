fcc5ba666a50f0bc5893c2305c1392f97ac3c034
Beniamino-Soccio <beniamino43@gmail.com>
2024-10-23 16:26:28 +0200

    fixed cards + end of ex.

--- resources/views/comics/comics.blade.php
+++ resources/views/comics/comics.blade.php
@@ -10,8 +10,8 @@
             @foreach ( $comics as $index => $singleComic)
 
                 <div class="col-3">
-                    <div class="card mb-3">
-                        <img src="{{$singleComic["thumb"]}}" class="card-img-top" alt="{{$singleComic["title"]}}">
+                    <div id="cards-comics" class="card mb-4">
+                        <img id="img-cards-comics" src="{{$singleComic["thumb"]}}" class="card-img-top" alt="{{$singleComic["title"]}}">
                         <div class="card-body text-center">
                             <h5 class="card-text fw-bold fs-6">{{$singleComic["title"]}}</h5>
                             <p class="card-text"> <span class="fw-bold">Price:</span>  {{$singleComic["price"]}}</p>
--- resources/views/comics/show.blade.php
+++ resources/views/comics/show.blade.php
@@ -5,16 +5,19 @@
 @section("main-content")
 
     <div class="container">
-        <h1 class="fw-bold text-center p-3">DC Comic</h1>
-        <h2 class="text-center mb-5">{{ $comic["title"] }}</h2>
+        <h1 class="fw-bold text-center">DC Comic</h1>
+        <h2 class="text-center m-3">{{ $comic["title"] }}</h2>
         <div class="row justify-content-center">
-            <div class="col-8">
+            <div class="col-7">
                 <div class="card mb-3">
                     <img src="{{$comic["thumb"]}}" class="card-img-top" alt="{{$comic["title"]}}">
-                    <div class="card-body text-center">
+                    <div class="card-body">
                         <h5 class="card-text fw-bold fs-6">{{$comic["title"]}}</h5>
+                        <p class="card-text"> <span class="fw-bold">Description:</span>  {{$comic["description"]}}</p>
                         <p class="card-text"> <span class="fw-bold">Price:</span>  {{$comic["price"]}}</p>
                         <p class="card-text"> <span class="fw-bold">Series:</span>  {{$comic["series"]}}</p>
+                        <p class="card-text"> <span class="fw-bold">Sale Date:</span>  {{$comic["sale_date"]}}</p>
+                        <p class="card-text"> <span class="fw-bold">Type:</span>  {{$comic["type"]}}</p>
                     </div>
                 </div>
             </div>
